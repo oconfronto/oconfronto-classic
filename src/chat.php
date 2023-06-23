03d626bba073ce57a53d90a1b6f2560e78364802
MarcosSemedo1 <115740986+MarcosSemedo@users.noreply.github.com>
2023-06-23 12:55:32 -0300

    Update chat.php
    Alterado o style do chat para ocupar a tela toda, e removido os links que jogava para o site do xat.com

--- src/chat.php
+++ src/chat.php
@@ -6,6 +6,6 @@ $player = check_user($secret_key, $db);
 include("templates/chat_header.php");
 ?>
 <center>
-<iframe src="https://xat.com/embed/chat.php#id=220522347&gn=oconfrontorpg1234" width="540" height="405" frameborder="0" scrolling="no"></iframe><br><small><a target="_BLANK" href="https://xat.com/web_gear/chat/embed.php?id=220522347&GroupName=oconfrontorpg1234">Get oconfrontorpg1234 chat group</a> | <a target="_BLANK" href="https://xat.com/oconfrontorpg1234"> Go to oconfrontorpg1234 website</a></small><br>
+<iframe style="position:fixed; top:0; left:0; right:0; bottom:0; width: 100%; height: 100%" src="https://xat.com/embed/chat.php#id=220522347&gn=oconfrontorpg1234" width="540" height="405" frameborder="0" scrolling="no"></iframe>
 </body>
 </html>
