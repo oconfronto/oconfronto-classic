59080016e692d21fda945a7ade6ce2c6d08c8d27
MarcosSemedo1 <115740986+MarcosSemedo@users.noreply.github.com>
2023-06-23 13:00:30 -0300

    Update chat.php
    Retirado o <center> pois é antigo e não mais utilizado.

--- src/chat.php
+++ src/chat.php
@@ -5,7 +5,7 @@ $player = check_user($secret_key, $db);
 
 include("templates/chat_header.php");
 ?>
-<center>
+
 <iframe src="https://xat.com/embed/chat.php#id=220522347&gn=oconfrontorpg1234" width="540" height="405" frameborder="0" scrolling="no"></iframe><br><small><a target="_BLANK" href="https://xat.com/web_gear/chat/embed.php?id=220522347&GroupName=oconfrontorpg1234">Get oconfrontorpg1234 chat group</a> | <a target="_BLANK" href="https://xat.com/oconfrontorpg1234"> Go to oconfrontorpg1234 website</a></small><br>
 </body>
 </html>
