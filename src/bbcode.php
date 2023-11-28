bea62596073ccb51c11b9cc55201b67af00eebe8
Jean Ribeiro <contact@jeanribeiro.dev>
2023-11-27 23:40:04 -0300

    fix bbcode.php

--- src/bbcode.php
+++ src/bbcode.php
@@ -4,7 +4,7 @@ class bbcode
 {
     public function code_box($text)
     {
-        return "<blockquote><b><font size=1>Código:</font></b><br/>" . $text . "</blockquote>";
+        return "<blockquote><b><font size=1>Código:</font></b><br/>" . htmlentities($text) . "</blockquote>";
     }
 
     public function quote($text)
@@ -27,22 +27,22 @@ class bbcode
 
         // Pattern and replacements
         $patternsAndReplacements = [
-            "/\\[b\\](.+?)\[\/b\]/is" => '<b>\1</b>',
-            "/\\[i\\](.+?)\[\/i\]/is" => '<i>\1</i>',
-            "/\\[u\\](.+?)\[\/u\]/is" => '<u>\1</u>',
-            "/\\[center\\](.+?)\[\/center\]/is" => '<center>\1</center>',
-            "/\\[left\\](.+?)\[\/left\]/is" => '<div align=left>\1</div>',
-            "/\\[right\\](.+?)\[\/right\]/is" => '<div align=right>\1</div>',
-            "/\[s\](.+?)\[\/s\]/is" => '<s>\1</s>',
-            "/\[small\](.+?)\[\/small\]/is" => '<font size="1px">\1</font>',
-            "/\[big\](.+?)\[\/big\]/is" => '<font size="5px">\1</font>',
-            "/\\[li\\](.+?)\[\/li\]/is" => '<li>\1</li>',
-            "/\\[order\\](.+?)\[\/order\]/is" => '<ol>\1</ol>',
-            "/\\[list\\](.+?)\[\/list\]/is" => '<ul>\1</ul>',
-            "/\\[img\\]([^\\[]*)\\[/img\\]/i" => '<img style="max-width:460px; width: expression(this.width > 460 ? 460: true);" src="\1">',
-            "/\\[url\\]([^\\[]*)\\[/url\\]/i" => '<a>\1</a>',
-            "/\\[color=([^\\[]*)\\]([^\\[]*)\\[/color\\]/i" => '<font color="\1">\2</font>',
-            "/\\[url=([^\\[]*)\\]([^\\[]*)\\[/url\\]/i" => '<a href="\1" target="_blank" border="0px">\2</a>',
+            "/\\[b\\](.+?)\\[\\/b\\]/is" => '<b>\1</b>',
+            "/\\[i\\](.+?)\\[\\/i\\]/is" => '<i>\1</i>',
+            "/\\[u\\](.+?)\\[\\/u\\]/is" => '<u>\1</u>',
+            "/\\[center\\](.+?)\\[\\/center\\]/is" => '<center>\1</center>',
+            "/\\[left\\](.+?)\\[\\/left\\]/is" => '<div align="left">\1</div>',
+            "/\\[right\\](.+?)\\[\\/right\\]/is" => '<div align="right">\1</div>',
+            "/\\[s\\](.+?)\\[\\/s\\]/is" => '<s>\1</s>',
+            "/\\[small\\](.+?)\\[\\/small\\]/is" => '<font size="1px">\1</font>',
+            "/\\[big\\](.+?)\\[\\/big\\]/is" => '<font size="5px">\1</font>',
+            "/\\[li\\](.+?)\\[\\/li\\]/is" => '<li>\1</li>',
+            "/\\[order\\](.+?)\\[\\/order\\]/is" => '<ol>\1</ol>',
+            "/\\[list\\](.+?)\\[\\/list\\]/is" => '<ul>\1</ul>',
+            "/\\[img\\]([^\\[]*)\\[\\/img\\]/i" => '<img style="max-width:460px; width: expression(this.width > 460 ? 460: true);" src="\1">',
+            "/\\[url\\]([^\\[]*)\\[\\/url\\]/i" => '<a href="\1">\1</a>',
+            "/\\[color=([^\\[]*)\\]([^\\[]*)\\[\\/color\\]/i" => '<font color="\1">\2</font>',
+            "/\\[url=([^\\[]*)\\]([^\\[]*)\\[\\/url\\]/i" => '<a href="\1" target="_blank">\2</a>',
         ];
 
         foreach ($patternsAndReplacements as $pattern => $replacement) {
@@ -50,8 +50,8 @@ class bbcode
         }
 
         // Special cases with callbacks
-        $text = preg_replace_callback("/\[code\](.+?)\[\/code\]/is", function($matches) { return $this->code_box($matches[1]); }, $text);
-        $text = preg_replace_callback("/\[quote\](.+?)\[\/quote\]/is", function($matches) { return $this->quote($matches[1]); }, $text);
+        $text = preg_replace_callback("/\\[code\\](.+?)\\[\\/code\\]/is", function($matches) { return $this->code_box($matches[1]); }, $text);
+        $text = preg_replace_callback("/\\[quote\\](.+?)\\[\\/quote\\]/is", function($matches) { return $this->quote($matches[1]); }, $text);
 
         // Smilies
         $smilies = [
