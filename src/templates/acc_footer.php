db560aae1815e48f386b0dd66f3b08e881e73d77
Jean Ribeiro <contact@jeanribeiro.dev>
2023-12-01 22:40:23 -0300

    updates acc_footer.php

--- src/templates/acc_footer.php
+++ src/templates/acc_footer.php
@@ -2,22 +2,28 @@
 </div>
 
 <div id="footer">
-<div id="baixo-text"><font size="1">Copyright © 2008-2009 OC Productions</font></div>
+    <div id="baixo-text">
+        <font size="1">Copyright Â© 2023 jmr.technology</font>
+    </div>
 </div>
 </div>
-</td></tr></table>
-  
+</td>
+</tr>
+</table>
+
 
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 <script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-6607673-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+    try {
+        var pageTracker = _gat._getTracker("UA-6607673-1");
+        pageTracker._trackPageview();
+    } catch (err) {}
+</script>
 
 
 </body>
+
 </html>
